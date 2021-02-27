633ce2f448e24689e0a4d8509d5f554a8bd12e2a
Talha Mansoor <talhaulhaque53@gmail.com>
2021-02-27 12:25:32 +0500

    Persist timeout config update

--- redux/store.js
+++ redux/store.js
@@ -1,33 +1,26 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { persistStore, persistReducer } from 'redux-persist'
-import AsyncStorage from '@react-native-community/async-storage'
-
+import { persistStore, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-community/async-storage";
 
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-
-
 const persistedConfig = {
   key: "root",
-  storage: AsyncStorage
+  storage: AsyncStorage,
+  timeout: 0,
 };
 
-const persistedReducer = persistReducer(
-  persistedConfig,
-  rootReducer
-);
-
+const persistedReducer = persistReducer(persistedConfig, rootReducer);
 
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
   persistedReducer,
   initialState,
-  compose( applyMiddleware(...middleware),)
+  compose(applyMiddleware(...middleware))
 );
 
 const persistor = persistStore(store);
-
 
 export { persistor, store };
